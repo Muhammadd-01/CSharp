69659b06a087ec4004ea2a6de5d62c6d65554c43
Muhammad Affan <muhammadaffan1445@gmail.com>
2024-12-20 10:46:36 +0500

    Add project files.

---
+++ Error_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Error_01
+{
+     class Class1
+    {
+
+        string name = "ali";
+        int age = 12;
+        public void data()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 a = new Class1();
+            a.data();
+
+        }
+    }
+}
